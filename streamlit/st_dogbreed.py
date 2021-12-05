d448b082817b5e923f4a3f3ee9a0b6108a82778a
titipata <my.titipat@gmail.com>
2021-12-06 01:04:42 +0700

    Quick fix file name

--- streamlit/st_dogbreed.py
+++ streamlit/st_dogbreed.py
@@ -8,26 +8,29 @@ import torch
 import torch.nn as nn
 from torchvision import models, transforms
 
-transform = transforms.Compose([
-    transforms.Resize((299, 299)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+transform = transforms.Compose(
+    [
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
 n_classes = 120  # number of breeds classes
 model = models.inception_v3(pretrained=True)
 model.fc = nn.Sequential(
-    nn.Linear(2048, 512),
-    nn.ReLU(),
-    nn.Dropout(0.3),
-    nn.Linear(512, n_classes)
+    nn.Linear(2048, 512), nn.ReLU(), nn.Dropout(0.3), nn.Linear(512, n_classes)
 )
-if not op.exists('inception_v3_weights.pth'):
-    wget.download('https://detecting-scientific-claim.s3.us-west-2.amazonaws.com/inception_dog_breed.pt')
-model.load_state_dict(torch.load('inception_dog_breed.pt'))
+if not op.exists("inception_dog_breed.pt"):
+    wget.download(
+        "https://detecting-scientific-claim.s3.us-west-2.amazonaws.com/inception_dog_breed.pt"
+    )
+else:
+    model.load_state_dict(torch.load("inception_dog_breed.pt"))
 
 class_to_idx = json.load(open("class_to_idx.json", "r"))
 idx_to_class = {v: k for k, v in class_to_idx.items()}
+
 
 def predict(path: str):
     """Predict from a given path"""
@@ -43,7 +46,7 @@ st.title("Dog Breed Classification")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
     st.write("")
     st.write("Classifying...")
     pred = predict(uploaded_file)
